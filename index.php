c5113f9e50c242b553f25baea28797eeedbd3cf9
ASIF AHMAD <asifahma7761@gmail.com>
2024-06-08 23:05:20 +0530

    php practise add

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+echo " <center><h1 style='color:green;'>hii, my name is anam</h1></center>"; //single line comment
+/*$a=5;
+$b=6;
+$c=$a+$b; // multy line comment
+echo"<br>", $c;
+*/
+
+//Arthimatict orpator and varible 
+// $a = 10; // ($a and $b) indecate varible
+// $b = 5;
+
+ 
+// echo " 10 + 5 = ", $a+$b , "<br>";
+// echo " 10 - 5 = ", $a-$b, "<br>";
+// echo " 10 * 5 = ", $a*$b, "<br>";
+// echo " 10 / 5 = ", $a/$b, "<br><br>";
+
+//Asinment orpator and varible, asine value to a varible
+ 
+// $a +=2;
+// echo  $a, "+", $b," = ", $a+$b , "<br>";
+// $a -=2;
+// echo $a, "+", $b," = ", $a-$b, "<br>";
+// $a *=2;
+// echo $a, "+", $b," = ", $a*$b, "<br>";
+// $a /=2;
+// echo $a, "+", $b," = ", $a/$b, "<br>"; 
+
+
+//camperison oprator and varible, asine value to a varible
+
+$first_varible = 5;
+$second_varible = 6;
+
+
+echo var_dump( $first_varible == $second_varible), "<br>";
+echo var_dump( $first_varible <= $second_varible), "<br>";
+echo var_dump( $first_varible >= $second_varible), "<br>";
+echo var_dump( $first_varible != $second_varible), "<br>";
+
+
+
+?>
+
+
+    
+</body>
+</html>
